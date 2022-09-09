7fde6b28595e7e42b460b1720db9e67946dc0921
aemtea <37565509+aemtea@users.noreply.github.com>
2022-09-09 16:17:40 -0500

    wire up basic socket io implementation

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const http = require('http');
 const { Server } = require("socket.io");
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const io = new Server(server);
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:19006"
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
+
+server.listen(3000);
